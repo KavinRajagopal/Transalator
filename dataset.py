c6fae1b56cebdbc31661a2e4bdb8f1c399d33662
kavinrajagopal99@gmail.com <kavinrajagopal@MacBook-Pro-2.attlocal.net>
2024-10-03 15:33:51 -0500

    Training loop with saving model and logging model to tensorboard has been added

--- dataset.py
+++ dataset.py
@@ -20,9 +20,7 @@ class BilingualDataset(Dataset):
     def __len__(self):
         return len(self.ds)
     
-    def casual_mask(seq_len):
-        mask = torch.triu(torch.ones(seq_len, seq_len), diagonal = 1).type(torch.int)
-        return mask == 0
+ 
     
     def __getitem__(self, idx: Any) -> Any:
         src_target_pair = self.ds[idx]
@@ -76,11 +74,17 @@ class BilingualDataset(Dataset):
         return {
             "encoder_input": encoder_input, #seq_len
             "decoder_input": decoder_input, #seq_len
-            "encoder_mask": [encoder_input != self.pad_token].unsqueeze(0).unsqueeze(0).int(), #seq_len
-            "decoder_mask": [decoder_input != self.pad_token ].unsqueeze(0).unsqueeze(0).int() & casual_mask(decoder_input.size(0)) #seq_len
-            "label": label
-
+            "encoder_mask": (encoder_input != self.pad_token).unsqueeze(0).unsqueeze(0).int(), #seq_len
+            "decoder_mask": (decoder_input != self.pad_token).unsqueeze(0).unsqueeze(0).int() & casual_mask(decoder_input.size(0)), #seq_len
+            "label": label, #seq_len
+            "src_text": src_text,
+            "tgt_text": tgt_text
         }
+
+    
+def casual_mask(seq_len):
+    mask = torch.triu(torch.ones(seq_len, seq_len), diagonal = 1).type(torch.int)
+    return mask == 0
     
     
     
--- train.py
+++ train.py
@@ -2,8 +2,11 @@ import torch
 import torch.nn as nn 
 from torch.utils.data import Dataset, DataLoader, random_split
 from model import build_transformer
+from tqdm import tqdm
 
 from dataset import BilingualDataset , casual_mask
+
+from config import get_weights_file_path, get_config
 
 from datasets import load_dataset   
 from tokenizers import Tokenizer
@@ -67,5 +70,97 @@ def get_ds(cofig):
 def get_model(config, vocab_src_len, vocab_tgt_len):
     model = build_transformer(vocab_src_len, vocab_tgt_len, config['seq_len'], config['seq_len'], config['d_model'])
     return model
+
+
+def train_model(config):
+    #define model 
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f'Using device: {device}')
+
+    Path(config['model_folder']).mkdir(parents = True, exist_ok = True)
+
+    train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt = get_ds(config)
+    model = get_model(config, tokenizer_src.get_vocab_size(), tokenizer_tgt.get_vocab_size()).to(device)
+
+    #tensorboard
+    writer = SummaryWriter(config['experiment_name'])
+
+    optimizer = torch.optim.Adam(model.parameters(), lr = config['lr'], eps = 1e-9)
+
+    initial_epoch = 0
+    global_step = 0
+
+    if config['preload']:
+        model_filename = get_weights_file_path(config, config['preload'])
+        print(f'Preloading model from {model_filename}')
+        state = torch.load(model_filename)
+        initial_epoch = state['epoch'] + 1
+        optimizer.load_state_dict(state['optimizer_state_dict'])
+        global_step = state['global_step']
+
+    loss_fn = nn.CrossEntropyLoss(ignore_index = tokenizer_tgt.token_to_id("[PAD]"), label_smoothing = 0.1).to(device)
+
+    for epoch in range(initial_epoch, config['num_epochs']):
+        model.train()
+        batch_iterator = tqdm(train_dataloader, desc = f'Processing Epoch {epoch}', position = 0, leave = True)
+        for batch in batch_iterator:
+
+
+            encoder_input = batch['encoder_input'].to(device) # [batch_size, seq_len]
+            decoder_input = batch['decoder_input'].to(device) # [batch_size, seq_len]
+
+            encoder_mask = batch['encoder_mask'].to(device)   # [batch_size, 1, 1, seq_len]
+            decoder_mask = batch['decoder_mask'].to(device)   # [batch_size, 1, seq_len, seq_len]
+
+
+            #run the tensors through the transformers
+            encoder_input = model.encode(encoder_input, encoder_mask) # [batch_size, seq_len, d_model]
+            decoder_input = model.decode(encoder_output, encoder_mask, decoder_input, decoder_mask) # [batch_size, seq_len, d_model]
+            proj_output = model.projection(decoder_output) # [batch_size, seq_len, vocab_tgt_len]
+
+
+            label = batch['label'].to(device) # [batch_size, seq_len]
+
+
+
+            #(batch_size ,seq_len, vocab_tgt_len) -> (batch_size * seq_len)
+
+            loss = loss_fn(proj_output.view(-1, tokenizer_tgt.get_vocab_size()), label.view(-1))
+            batch_iterator.set_postfix({f"loss": f"{loss.item():6.3f}"})
+
+            #Log the loss in tensorboard 
+
+            write.add_scalar('Loss/train', loss.item(), global_step)
+            writer.flush()
+
+            #backpropagation
+            loss.backward()
+
+            #update the weights
+            optimizer.step()
+            optimizer.zero_grad()
+
+
+            global_step += 1
+
+        #save the model at the end of every epoch 
+        model_filename = get_weights_file_path(config, f'{epoch:02d}')
+
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'global_step': global_step
+        }, model_filename)
+
+if __name__ == "__main__":
+    Warning.filterwarnings("ignore")
+    config = get_config()
+    train_model(config)
+
+
+
+
+
 
 
